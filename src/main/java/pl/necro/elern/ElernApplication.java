b8dc7a77c9b30e0d312eb31f2e61e047773717ac
Kacper Gmurczyk <47670007+KacperG98@users.noreply.github.com>
2020-08-23 22:20:40 +0200

    project structure

---
+++ src/main/java/pl/necro/elern/ElernApplication.java
@@ -0,0 +1,13 @@
+package pl.necro.elern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElernApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElernApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/necro/elern/ElernApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.necro.elern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElernApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
